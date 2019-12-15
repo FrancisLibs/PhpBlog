d31d01ad062659e6e56ed30713bba1ae3559c8c5
francisL <fr.libs@gmail.com>
2019-12-15 11:02:34 +0100

    Ajout de la classe dess formulaires de Posts

---
+++ lib/vendors/FormBuilder/PostsFormBuilder.php
@@ -0,0 +1,50 @@
+<?php
+namespace FormBuilder;
+
+use \OCFram\FormBuilder;
+use \OCFram\StringField;
+use \OCFram\TextField;
+use \OCFram\MaxLengthValidator;
+use \OCFram\NotNullValidator;
+
+class PostsFormBuilder extends FormBuilder
+{
+  public function build()
+  {
+    $this->form->add(new StringField([
+        'label' => 'Auteur',
+        'name' => 'auteur',
+        'maxLength' => 20,
+        'validators' => [
+          new MaxLengthValidator('L\'auteur spécifié est trop long (20 caractères maximum)', 20),
+          new NotNullValidator('Merci de spécifier l\'auteur de la news'),
+        ],
+       ]))
+       ->add(new StringField([
+        'label' => 'Titre',
+        'name' => 'titre',
+        'maxLength' => 100,
+        'validators' => [
+          new MaxLengthValidator('Le titre spécifié est trop long (100 caractères maximum)', 100),
+          new NotNullValidator('Merci de spécifier le titre de la news'),
+        ],
+       ]))
+       ->add(new StringField([
+        'label' => 'Chapeau',
+        'name' => 'chapeau',
+        'maxLength' => 255,
+        'validators' => [
+          new MaxLengthValidator('Le chapo spécifié est trop long (255 caractères maximum)', 255),
+          new NotNullValidator('Merci de spécifier le chapo du post'),
+        ],
+       ->add(new TextField([
+        'label' => 'Contenu',
+        'name' => 'contenu',
+        'rows' => 8,
+        'cols' => 60,
+        'validators' => [
+          new NotNullValidator('Merci de spécifier le contenu de la news'),
+        ],
+       ]));
+  }
+}
