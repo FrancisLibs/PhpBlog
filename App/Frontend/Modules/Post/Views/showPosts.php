7c177026f25553d1cbd90de2b54b131bfb0ce327
francis <fr.libs@gmail.com>
2020-02-20 18:34:12 +0100

    Change the showPosts file, add htmlspecialchars to output

--- App/Frontend/Modules/Post/Views/showPosts.php
+++ App/Frontend/Modules/Post/Views/showPosts.php
@@ -9,9 +9,9 @@
             <div class="front-showPosts-title">
               <h3><?= htmlspecialchars($post['title']) ?></h3>
 <<<<<<< HEAD
-              <p class="attributsPost"> de : <?= htmlspecialchars(nl2br($post['autor_name'])) ?></p>
+              <p class="attributsPost"> de : <?= nl2br(htmlspecialchars($post['autor_name'])) ?></p>
 =======
-              <p class="attributsPost"> de <?= htmlspecialchars(nl2br($post['autor_name'])) ?></p>
+              <p class="attributsPost"> de <?= nl2br(htmlspecialchars($post['autor_name'])) ?></p>
 >>>>>>> security
               <?php if(isset($post['update_date']))
               { ?>
@@ -20,7 +20,7 @@
               <a class="btn-lire btn btn-info btn-xs" role="button" href="post-<?= $post['id'] ?>.html">Lire cet article</a>
             </div>
             <div class="front-showPosts-chapo">
-              <p class="chapo"><?= htmlspecialchars(nl2br($post['chapo'])) ?></p>
+              <p class="chapo"><?= nl2br(htmlspecialchars($post['chapo'])) ?></p>
             </div>
           </td>
         </tr>
